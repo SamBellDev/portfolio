2771f1ce7e7a8b99fce5038ae4bcf0004b4835d7
Sam Bell <sambelldev@tuta.io>
2021-10-14 14:05:01 +0100

    added routing module and started bones of fodler structure

---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to my website!</h1>
+            <p>Here you will find all of my programming projects and crochet portfolio</p>
+        </div>
+    )
+}
+
+export default HomePage
