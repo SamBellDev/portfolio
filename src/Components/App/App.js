7aa54da6e73ed7f88c78d8f7b53846edd643fe0b
Sam Bell <sambelldev@tuta.io>
2021-10-15 15:27:55 +0100

    used a Bootstrap Nav instead. Implemented Nav but now need to add individual project components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,5 @@
 import MyRouter from '../MyRouter/MyRouter'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
--- src/Components/MyRouter/MyRouter.js
+++ src/Components/MyRouter/MyRouter.js
@@ -1,14 +1,34 @@
 import {Route, Switch, BrowserRouter as Router} from 'react-router-dom'
-import Navbar from '../Navbar/Navbar'
-import HomePage from "../Homepage/Homepage";
-import Contact from "../Contact/Contact";
-import NotFound from "../NotFound/NotFound";
-import ProjectsPage from "../ProjectsPage/ProjectsPage";
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import HomePage from '../Homepage/Homepage';
+import Contact from '../Contact/Contact'
+import NotFound from '../NotFound/NotFound'
+import ProjectsPage from '../ProjectsPage/ProjectsPage'
 
 const MyRouter = () => {
     return (
         <Router>
-            <Navbar />
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/">Sam Bell Dev</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <NavDropdown title="Projects" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="/projects">All projects</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.2">Project 1</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Project 2</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.4">Project 3</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="/contact">Contact me</Nav.Lin
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
             <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route path="/projects" component={ProjectsPage} />
